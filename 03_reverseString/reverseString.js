36a5b5c69c2b85ad35eb7a994bbba667d64301a4
MESSENGER1337 <186360316+MESSENGER1337@users.noreply.github.com>
2024-12-04 02:08:42 -0600

    Complete 03_reverseString javaScript exercise
    i completed the reverseString exercise
    all tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let newWord = word.split('');
+    let reversed = newWord.reverse();
+    let result = reversed.join('');
+    return result;
 
 };
 
+reverseString('123! abc! Hello, Odinite.')
 // Do not edit below this line
 module.exports = reverseString;
+
+
+// npm test reverseString.spec.js
