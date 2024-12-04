6992927fda33eda2d63bd1cf25640a1bc1f9db95
MESSENGER1337 <186360316+MESSENGER1337@users.noreply.github.com>
2024-12-04 01:35:53 -0600

    Complete repeatString exercise
    I finished all of the tests in the repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+    if (times < 0) return 'ERROR';
+    let result = '';
+    for (let i = 0; i < times; i++) {
+        result += word;
+    }
+    return result;
 };
+
+repeatString('odin', 300)
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
+// npm test repeatString.spec.js
