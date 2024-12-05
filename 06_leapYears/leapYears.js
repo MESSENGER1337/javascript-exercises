a4a714daaaaf0265e3de97d105b1ff364210dc7c
MESSENGER1337 <186360316+MESSENGER1337@users.noreply.github.com>
2024-12-05 17:30:00 -0600

    Complete Leap Year exercise
    completed the leap year javascript exercise with an arrow function.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
+// const leapYears = function(year) {
+//     let result = false;
+//     if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) result = true;
+//     return result;
+// };
 
-};
 
+
+
+// const leapYears = function(year) {
+//     return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+// };
+
+
+
+const leapYears = (year) => (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+
+
+
+leapYears(1600);
 // Do not edit below this line
 module.exports = leapYears;
+
+// npm test leapYears.spec.js
