9bc1fc9e9416b0eb93ad0f967f5308d2d63c8680
MESSENGER1337 <186360316+MESSENGER1337@users.noreply.github.com>
2024-12-04 19:32:48 -0600

    Complete removeFromArray javaScript exercise
    wrote a function that removes elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    return array.filter(item => !elementsToRemove.includes(item));
 };
 
+removeFromArray([1, 2, 3], "1", 3);
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// npm test removeFromArray.spec.js
